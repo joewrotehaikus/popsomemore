e43b8d1756dfde703013a5aa3b1ec8e908423ba4
Anug Saha <anugsaha@gmail.com>
2021-08-09 12:31:31 -0400

    create opening screen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const openingScreen = document.querySelector("#opening");
 const canvas = document.getElementById("balloon-game");
 const game = new PopSomeMore(canvas);
 
-// debugger
 window.onkeydown = function (e) {
   if (e.keyCode === 13) {
     openingScreen.style.display = "none";
